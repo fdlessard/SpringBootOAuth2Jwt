b7203fad77263ce9e51d05ac0945aeb1da9359a5
fdlessard <fdlessard@gmail.com>
2019-12-20 19:37:24 -0500

    ¨refactoring

--- authorization-server/src/it/java/io/fdlessard/codebites/oauth2jwt/authorization/UserIt.java
+++ authorization-server/src/it/java/io/fdlessard/codebites/oauth2jwt/authorization/UserIt.java
@@ -1,57 +1,65 @@
 package io.fdlessard.codebites.oauth2jwt.authorization;
 
 
-import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.MEDIA_TYPE_APPLICATION_HAL_JSON;
-import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.TEST_AUTHORIZATION;
-import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.TEST_BEARER_STR;
-import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.TEST_CLIENT_ID;
-import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.TEST_CLIENT_SECRET;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static io.fdlessard.codebites.oauth2jwt.authorization.TestConstants.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserIt extends BaseIt {
+public class UserIt {
 
-  @BeforeEach
-  void setup() throws Exception {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
-        .addFilter(springSecurityFilterChain).build();
-    accessToken = obtainAccessToken(TEST_CLIENT_ID, TEST_CLIENT_SECRET);
-  }
+    @Autowired
+    private WebApplicationContext wac;
 
-  @Test
-  void getUsers() throws Exception {
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
 
-    mockMvc.perform(get("/users")
-        .header(TEST_AUTHORIZATION, TEST_BEARER_STR + accessToken))
-        .andExpect(status().is2xxSuccessful())
-        .andExpect(content().contentType(MEDIA_TYPE_APPLICATION_HAL_JSON))
-        .andExpect(jsonPath("$._embedded.users", hasSize(2)))
-        .andExpect(jsonPath("$._embedded.users[0].id", is(1)))
-        .andExpect(jsonPath("$._embedded.users[0].password").doesNotExist());
-  }
+    private MockMvc mockMvc;
 
-  @Test
-  void getUser() throws Exception {
 
-    mockMvc.perform(get("/users/1")
-        .header(TEST_AUTHORIZATION, TEST_BEARER_STR + accessToken))
-        .andExpect(status().is2xxSuccessful())
-        .andExpect(content().contentType(MEDIA_TYPE_APPLICATION_HAL_JSON))
-        .andExpect(jsonPath("$.id", is(1)))
-        .andExpect(jsonPath("$.password").doesNotExist());
+    @BeforeEach
+    void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .addFilter(springSecurityFilterChain).build();
+    }
 
-  }
+    @Test
+    void getUsers() throws Exception {
+
+        mockMvc.perform(get("/users")
+                .with(httpBasic(TEST_USERNAME, TEST_PASSWORD_STR)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MEDIA_TYPE_APPLICATION_HAL_JSON))
+                .andExpect(jsonPath("$._embedded.users", hasSize(1)))
+                .andExpect(jsonPath("$._embedded.users[0].username", is(TEST_USERNAME)))
+                .andExpect(jsonPath("$._embedded.users[0].password").doesNotExist());
+    }
+
+    @Test
+    void getUser() throws Exception {
+
+        mockMvc.perform(get("/users/0")
+                .with(httpBasic(TEST_USERNAME, TEST_PASSWORD_STR)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MEDIA_TYPE_APPLICATION_HAL_JSON))
+                .andExpect(jsonPath("$.username", is(TEST_USERNAME)))
+                .andExpect(jsonPath("$.password").doesNotExist());
+
+    }
 }
